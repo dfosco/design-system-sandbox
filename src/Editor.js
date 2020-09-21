6f68aeeb589b0b72e3e89e5da84fefc5821f2b76
Daniel Fosco <daniel.fosco@booking.com>
2020-09-21 16:16:48 +0200

    Add simple routing

--- src/Editor.js
+++ src/Editor.js
@@ -3,15 +3,17 @@ import CoreEditor from "./CoreEditor";
 import nightOwl from "prism-react-renderer/themes/nightOwl";
 import PropTypes from "prop-types";
 
-export default function Editor({ code, scope }) {
+export default function Editor({ code, scope, tag }) {
+  const TagName = tag || 'div';
   return (
-    <main className="bg-dark-gray flex-auto flex flex-column flex-row-l h-100">
+    <TagName className="bg-dark-gray flex-auto flex flex-column flex-row-l h-100">
       <CoreEditor theme={nightOwl} code={code} scope={scope} />
-    </main>
+    </TagName>
   );
 }
 
 Editor.propTypes = {
   code: PropTypes.string,
-  scope: PropTypes.object
+  scope: PropTypes.object,
+  tag: PropTypes.string
 };
